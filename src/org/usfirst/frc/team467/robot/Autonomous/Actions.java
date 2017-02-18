63934329cd023650bb4f18427e3c6b2b861b3296
Nathan Petrangelo <nathan@petrangelo.org>
2017-02-17 19:51:08 -0500

    Added Aim Action

--- src/org/usfirst/frc/team467/robot/Autonomous/Actions.java
+++ src/org/usfirst/frc/team467/robot/Autonomous/Actions.java
@@ -3,14 +3,19 @@ package org.usfirst.frc.team467.robot.Autonomous;
 import org.usfirst.frc.team467.robot.*;
 
 public class Actions {
+	private static Drive drive = Drive.getInstance();
 	
 	public static Action example = new Action("Example", new Process.Duration(1000), () -> {
 		System.out.println("Running Example Autonomous Action: " + System.currentTimeMillis());
 	});
 	
 	public static Action moveForward = new Action("Move Forward 1 second", new Process.Duration(1000), () -> {
-		Drive.getInstance().crabDrive(0, 1.0);
+		drive.crabDrive(0, 1.0);
 	});
+	
+	public static Action aim(double angle) {
+		return new Action("Aim", () -> {return drive.aiming.onTarget();}, () -> {drive.turnToAngle(angle);});
+	}
 	
 	public static Process getExampleProcess()
 	{
