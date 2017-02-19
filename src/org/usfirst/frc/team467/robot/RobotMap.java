c97fd5c5cefce5f8d0a0ee5a039933fc6b8fc716
Callan McInally <callan@famulus.com>
2017-02-19 14:10:45 -0500

    Cleanup formatting of RobotMap file.  Note use of eclipse @formatter contracts to avoid formatting certain sections

--- src/org/usfirst/frc/team467/robot/RobotMap.java
+++ src/org/usfirst/frc/team467/robot/RobotMap.java
@@ -1,23 +1,12 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.usfirst.frc.team467.robot;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
  */
 public class RobotMap {
 	// Global robot constants
-	/**
-	 * The maximum revolutions per minute (RPM) of a wheel when in speed control mode.
-	 */
+	
+	// The maximum revolutions per minute (RPM) of a wheel when in speed control mode.
 	public static final double MAX_SPEED = 300.0;
 
 	//
@@ -50,8 +39,13 @@ public class RobotMap {
 	public static final int BACK_LEFT_MOTOR_CHANNEL = 3;
 	public static final int BACK_RIGHT_MOTOR_CHANNEL = 4;
 
-	public static final int[] DRIVING_MOTOR_CHANNELS = { FRONT_LEFT_MOTOR_CHANNEL, FRONT_RIGHT_MOTOR_CHANNEL,
-			BACK_LEFT_MOTOR_CHANNEL, BACK_RIGHT_MOTOR_CHANNEL };
+	// @formatter:off
+	public static final int[] DRIVING_MOTOR_CHANNELS = { 
+			FRONT_LEFT_MOTOR_CHANNEL, 
+			FRONT_RIGHT_MOTOR_CHANNEL,
+			BACK_LEFT_MOTOR_CHANNEL, 
+			BACK_RIGHT_MOTOR_CHANNEL 
+			};
 
 	// Invert the drive motors to allow for wiring.
 	public static final boolean FRONT_LEFT_DRIVE_INVERT = false;
@@ -59,9 +53,14 @@ public class RobotMap {
 	public static final boolean BACK_LEFT_DRIVE_INVERT = false;
 	public static final boolean BACK_RIGHT_DRIVE_INVERT = true;
 
-	public static final boolean[] IS_DRIVE_MOTOR_INVERTED = { FRONT_LEFT_DRIVE_INVERT, FRONT_RIGHT_DRIVE_INVERT,
-			BACK_LEFT_DRIVE_INVERT, BACK_RIGHT_DRIVE_INVERT };
-
+	public static final boolean[] IS_DRIVE_MOTOR_INVERTED = { 
+			FRONT_LEFT_DRIVE_INVERT, 
+			FRONT_RIGHT_DRIVE_INVERT,
+			BACK_LEFT_DRIVE_INVERT, 
+			BACK_RIGHT_DRIVE_INVERT 
+			};
+	// @formatter:on
+	
 	//
 	// Digital Inputs
 	//
@@ -81,47 +80,57 @@ public class RobotMap {
 	//
 
 	// Length is front to back, Width side to side
-	// Measured on 2015 robot
 	public static final double LENGTH = 31.5; // inches btw the wheels
 	public static final double WIDTH = 18.5; // inches btw the wheels
 
 	// Steering motor constant array
-	public static final int[] STEERING_MOTOR_CHANNELS = { RobotMap.FRONT_LEFT_STEERING_MOTOR_CHANNEL,
-			RobotMap.FRONT_RIGHT_STEERING_MOTOR_CHANNEL, RobotMap.BACK_LEFT_STEERING_MOTOR_CHANNEL,
-			RobotMap.BACK_RIGHT_STEERING_MOTOR_CHANNEL };
+	// @formatter:off
+	public static final int[] STEERING_MOTOR_CHANNELS = { 
+			RobotMap.FRONT_LEFT_STEERING_MOTOR_CHANNEL,
+			RobotMap.FRONT_RIGHT_STEERING_MOTOR_CHANNEL, 
+			RobotMap.BACK_LEFT_STEERING_MOTOR_CHANNEL,
+			RobotMap.BACK_RIGHT_STEERING_MOTOR_CHANNEL 
+			};
 
 	// Steering sensor constant array
-	public static final int[] STEERING_SENSOR_CHANNELS = { RobotMap.FRONT_LEFT_STEERING_SENSOR_CHANNEL,
-			RobotMap.FRONT_RIGHT_STEERING_SENSOR_CHANNEL, RobotMap.BACK_LEFT_STEERING_SENSOR_CHANNEL,
-			RobotMap.BACK_RIGHT_STEERING_SENSOR_CHANNEL };
+	public static final int[] STEERING_SENSOR_CHANNELS = { 
+			RobotMap.FRONT_LEFT_STEERING_SENSOR_CHANNEL,
+			RobotMap.FRONT_RIGHT_STEERING_SENSOR_CHANNEL, 
+			RobotMap.BACK_LEFT_STEERING_SENSOR_CHANNEL,
+			RobotMap.BACK_RIGHT_STEERING_SENSOR_CHANNEL 
+			};
 
 	// Data keys (names used when saving centers to robot)
-	public static final String[] STEERING_KEYS = new String[] { "FrontLeft", "FrontRight", "BackLeft", "BackRight" };
-
+	public static final String[] STEERING_KEYS = new String[] { 
+			"FrontLeft", 
+			"FrontRight", 
+			"BackLeft", 
+			"BackRight" 
+			};
+	// @formatter:on
+	
 	/**
 	 * Number of increments on the steering sensor (12-bit A/D)
 	 */
 	public static final double STEERING_RANGE = 4095;
 
 	// PID array
-	public static final PID[] PIDvalues = { new PID(-0.013, 0.0, 0.0), // Front
-																		// Left
-																		// PID
-																		// values
+	// @formatter:off
+	public static final PID[] PIDvalues = { 
+			new PID(-0.013, 0.0, 0.0), // Front Left PID values
 			new PID(-0.013, 0.0, 0.0), // Front Right PID values
 			new PID(-0.013, 0.0, 0.0), // Back Left PID values
 			new PID(-0.015, 0.0, 0.0), // Back Right PID values
 	};
 
-	public static final PID[] SpeedPIDFvalues = { new PID(0.50, 0.0036, 18.00, 2.35), // Front
-																						// Left
-																						// PID
-																						// values
+	public static final PID[] SpeedPIDFvalues = { 
+			new PID(0.50, 0.0036, 18.00, 2.35),  // Front Left PID values
 			new PID(1.35, 0.0027, 168.75, 1.90), // Front Right PID values
 			new PID(1.35, 0.0020, 168.75, 2.00), // Back Left PID values
 			new PID(1.35, 0.0027, 168.75, 2.00), // Back Right PID values
 	};
-
+	// @formatter:on
+	
 	public static final PID GearDevicePID = new PID(0.1, 0.01, 0.001, 0);
 
 	// TODO: need actual motor channels
@@ -133,14 +142,7 @@ public class RobotMap {
 	// TODO: need actual input device channel
 	public static final int GEAR_SENSOR_CHANNEL = 5;
 
-	/* distance from the center of the robot to the wheelpod in inches */
-	public static final double wheelpodRadius = 18.29;
-	/* diameter of wheelpod in inches */
-	public static final double wheeldiameter = 6;
-
-	/**
-	 * The number of encoder ticks per one revolution of the wheel. This is used for correctly determining RPM and position.
-	 */
+	// The number of encoder ticks per one revolution of the wheel. This is used for correctly determining RPM and position.
 	public static final int WHEELPOD_ENCODER_CODES_PER_REVOLUTION = 256;
 
 	/**
