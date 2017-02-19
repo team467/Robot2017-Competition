5e27bc9baecc9dbcc01268f9bc7a6199a8003c1e
Callan McInally <callan@famulus.com>
2017-02-19 12:43:50 -0500

    Code formatting cleanup to make it easier to compare code between branches

--- Test/org/usfirst/team467/robot/AutonomousTest.java
+++ Test/org/usfirst/team467/robot/AutonomousTest.java
@@ -6,12 +6,12 @@ import org.usfirst.frc.team467.robot.Autonomous.Process;
 
 public class AutonomousTest {
 	private Process autonomous = Actions.getExampleProcess();
-	
+
 	@Test
 	public void autonomousTest() {
 		while (true) {
 			autonomous.run();
 		}
 	}
-	
+
 }
