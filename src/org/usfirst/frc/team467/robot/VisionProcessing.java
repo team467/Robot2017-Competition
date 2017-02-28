d7442a7c470cd3874e8b995caa63f3d50551cf1c
Nathan Petrangelo <nathan@petrangelo.org>
2017-02-28 18:20:41 -0500

    Dedicated NetworkTable
    and Calculate Distance based on Height on Countour

--- src/org/usfirst/frc/team467/robot/VisionProcessing.java
+++ src/org/usfirst/frc/team467/robot/VisionProcessing.java
@@ -2,22 +2,23 @@ package org.usfirst.frc.team467.robot;
 
 import org.apache.log4j.Logger;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 public class VisionProcessing {
 	private static final Logger LOGGER = Logger.getLogger(VisionProcessing.class);
 	private static VisionProcessing instance;
 	
+	private NetworkTable table;
 	private Gyrometer gyro;
-	private Ultrasonic467 ultra;
-
+	
 	private double targetAngle, x, y, width, height = 0.0;
 	private boolean seeTwo = false;
 
 	private VisionProcessing() {
-		SmartDashboard.putNumber("CamToCenter", RobotMap.CamToCenterInches);
+		table = NetworkTable.getTable("Vision Table");
+		table.putNumber("CamToCenterWidth", RobotMap.CamToCenterWidthInches);
+		table.putNumber("CamToCenterLength", RobotMap.CamToCenterLengthInches);
 		gyro = Gyrometer.getInstance();
-		ultra = Ultrasonic467.getInstance();
 		update();
 	}
 
@@ -29,14 +30,13 @@ public class VisionProcessing {
 	}
 
 	public void update() {
-		seeTwo = SmartDashboard.getBoolean("seeTwo", false);
-		targetAngle = SmartDashboard.getNumber("angle", 0.0);
-		x = SmartDashboard.getNumber("x", 0.0);
-		y = SmartDashboard.getNumber("y", 0.0);
-		width = SmartDashboard.getNumber("w", 0.0);
-		height = SmartDashboard.getNumber("h", 0.0);
-		SmartDashboard.putNumber("ultra", ultra.getDistance());
-		SmartDashboard.putNumber("gyro", gyro.getRobotAngleDegrees());
+		seeTwo = table.getBoolean("seeTwo", false);
+		targetAngle = table.getNumber("angle", 0.0);
+		x = table.getNumber("x", 0.0);
+		y = table.getNumber("y", 0.0);
+		width = table.getNumber("w", 0.0);
+		height = table.getNumber("h", 0.0);
+		table.putNumber("gyro", gyro.getRobotAngleDegrees());
 		
 //		LOGGER.debug("Can see two contours = " + String.valueOf(seeTwo));
 	}
@@ -63,5 +63,19 @@ public class VisionProcessing {
 
 	public boolean canSeeTwo() {
 		return seeTwo;
+	}
+	
+	public double getDistance() {
+		return 3580/height;
+	}
+	
+	public void logDistanceValues() {
+		LOGGER.info("distance," + getHeight() + "," + 3580/getHeight() + "," + getHeight()*getWidth());
+	}
+	
+	@Override
+	public String toString() {
+		return "VisionProcessing [targetAngle=" + targetAngle + ", x=" + x + ", y=" + y + ", width=" + width
+				+ ", height=" + height + ", seeTwo=" + seeTwo + "]";
 	}
 }
