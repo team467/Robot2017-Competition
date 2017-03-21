048da8c9e36945379651adcf7d8b889047f8ab76
Nathan Petrangelo <nathan@petrangelo.org>
2017-03-21 19:41:41 -0400

    Added Current Limiting for Gear Device
    Also removed static declarations

--- src/org/usfirst/frc/team467/robot/GearDevice.java
+++ src/org/usfirst/frc/team467/robot/GearDevice.java
@@ -2,12 +2,16 @@ package org.usfirst.frc.team467.robot;
 
 import org.apache.log4j.Logger;
 
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.Spark;
 
 public class GearDevice {
-	private static final Logger LOGGER = Logger.getLogger(GearDevice.class);
+	private final Logger LOGGER = Logger.getLogger(GearDevice.class);
 	private static GearDevice instance = null;
-	private static Spark spark;
+	private Spark spark;
+	private PowerDistributionPanel pdp;
+	private final int pdpChannel = 11;
+	private CheckCurrentLimit limitChecker;
 
 	public static GearDevice getInstance() {
 		if (instance == null) {
@@ -16,19 +20,32 @@ public class GearDevice {
 		return instance;
 	}
 
-	// TODO: set to actual values of sensor
 	private GearDevice() {
 		spark = new Spark(RobotMap.GEAR_MOTOR);
+		pdp = new PowerDistributionPanel();
+		limitChecker = new CheckCurrentLimit(5, 3.5);
 	}
 
 	public void goDown() {
-		LOGGER.debug("Going Down");
-		spark.set(1);
+		final double current = pdp.getCurrent(pdpChannel);
+		LOGGER.debug("Going Down: current=" + current);
+		if (limitChecker.isOverLimit(current)) {
+			LOGGER.warn("*** OVER CURRENT LIMIT ***");
+			spark.set(0);
+		} else {
+			spark.set(1.0);
+		}
 	}
 
 	public void goUp() {
-		LOGGER.debug("Going Up");
-		spark.set(-1);
+		final double current = pdp.getCurrent(pdpChannel);
+		LOGGER.debug("Going Up: current=" + current);
+		if (limitChecker.isOverLimit(current)) {
+			LOGGER.warn("*** OVER CURRENT LIMIT ***");
+			spark.set(0);
+		} else {
+			spark.set(-1.0);
+		}
 	}
 
 }
